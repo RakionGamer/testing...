2d947408ad309fb5e0312c5759c6325a56209ab8
RakionG (Ricardo Valera) <101957005+RakionGamer@users.noreply.github.com>
2024-06-30 21:41:28 -0400

    first commit..

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const { exec } = require('child_process');
+var router = express.Router();
+// Endpoint para ejecutar truffle migrate
+router.post('/migrate', (req, res) => {
+  exec('truffle migrate --reset --network ganache', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error ejecutando truffle migrate: ${error}`);
+      return res.status(500).send(`Error ejecutando truffle migrate: ${stderr}`);
+    }
+    console.log(`truffle migrate output: ${stdout}`);
+    res.send(`truffle migrate completado: ${stdout}`);
+  });
+});
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
